527534617941b50172adc334e3dba3bcedcb491f
Jamie Bliss <jamie@ivyleav.es>
2021-11-14 17:40:34 -0500

    I think I have something working?

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+import asyncio
 import logging
+import pathlib
 
 from starlette.applications import Starlette
 from starlette.background import BackgroundTask
@@ -7,9 +9,15 @@ from starlette.routing import Route
 
 
 LOG = logging.getLogger(__name__)
+ROOT = pathlib.Path(__file__).absolute().parent
 
 #: The User ID of the user. eg, the last component of https://acme.pagerduty.com/users/PTUXL6G
 MY_ID = 'P5KJGS9'
+
+#: The files to play as an alarm
+FILES = [
+    ROOT / 'brass-bell.mp3',
+]
 
 
 # Incident status can be:
@@ -19,6 +27,7 @@ class IncidentTracker:
     def __init__(self):
         self._incidents = {}
         self._active_count = 0
+        self._play_task = None
 
     async def update(self, incident):
         iid = incident['id']
@@ -41,12 +50,50 @@ class IncidentTracker:
         old_count = self._active_count
         new_count = len(incs)
         self._active_count = new_count
-        LOG.debug("Old Count: %s, New Count: %s")
+        LOG.debug("Old Count: %s, New Count: %s", old_count, new_count)
 
         if old_count == 0 and new_count > 0:
             await self._trigger_alarm()
         elif old_count > 0 and new_count == 0:
             await self._silence_alarm()
+
+    async def _trigger_alarm(self):
+        """
+        Starts the alarm
+        """
+        LOG.info("Starting alarm")
+        self._play_task = asyncio.create_task(self._alarm_playback())
+
+    async def _silence_alarm(self):
+        """
+        Silences the alarm
+        """
+        LOG.info("Stopping alarm")
+        self._play_task.cancel()
+
+    async def _alarm_playback(self):
+        LOG.debug("VLC task started")
+        try:
+            proc = None
+            while True:
+                try:
+                    LOG.debug("Running VLC")
+                    proc = await asyncio.create_subprocess_exec(
+                        # VLC volume options have become confusing
+                        'cvlc', '--play-and-exit', '--gain', '8', *FILES,
+                    )
+                    await proc.wait()
+                    assert proc.returncode == 0
+                    proc = None
+                except asyncio.CancelledError:
+                    if proc is not None:
+                        proc.terminate()
+        
+                    raise
+        except Exception:
+            LOG.exception("Problem in VLC task")
+        else:
+            LOG.debug("VLC task exiting")
 
 
 async def homepage(request):
